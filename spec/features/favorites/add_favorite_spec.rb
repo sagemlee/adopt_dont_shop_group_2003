2f9e6bcd77654c1ebc082a692906fce1829f656d
Derek Borski <derek.borski@gmail.com>
2020-05-11 23:37:26 -0600

    Add favorite spec and model

--- spec/features/favorites/add_favorite_spec.rb
+++ spec/features/favorites/add_favorite_spec.rb
@@ -13,6 +13,30 @@ describe "When a user adds a favorite" do
 
     expect(page).to have_content("#{pet.name} has been added to your favorites.")
   end 
+  it "displays the total number of pets in favorites" do
+    
+    pet1 = create(:pet)
+    pet2 = create(:pet)
+    pet3 = create(:pet)
+
+    visit "/pets/#{pet1.id}"
+
+    click_button "Add Pet to Favorites"
+    
+    expect(page).to have_content("Favorited Pets: 1")
+    
+    visit "/pets/#{pet2.id}"
+
+    click_button "Add Pet to Favorites"
+    
+    expect(page).to have_content("Favorited Pets: 2")
+    
+    visit "/pets/#{pet3.id}"
+
+    click_button "Add Pet to Favorites"
+
+    expect(page).to have_content("Favorited Pets: 3")
+  end
 end
 
 # User Story 9, Favorite Creation
