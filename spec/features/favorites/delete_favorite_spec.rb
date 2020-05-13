4e17444795c77c89987d8489ae1012810118eefd
Derek Borski <derek.borski@gmail.com>
2020-05-12 23:12:37 -0600

    Add spec for having no favorite pets, update index view

--- spec/features/favorites/delete_favorite_spec.rb
+++ spec/features/favorites/delete_favorite_spec.rb
@@ -61,15 +61,3 @@ describe "After a user adds a favorite pet and visits the favorites page" do
     end
   end
 end
-
-# User Story 13, Remove a Favorite from Favorites Page
-
-# As a visitor
-# When I have added pets to my favorites list
-# And I visit my favorites page ("/favorites")
-# Next to each pet, I see a button or link to remove that pet from my favorites
-# When I click on that button or link to remove a favorite
-# A delete request is sent to "/favorites/:pet_id"
-# And I'm redirected back to the favorites page where I no longer see that
-# pet listed
-# And I also see that the favorites indicator has decremented by 1
