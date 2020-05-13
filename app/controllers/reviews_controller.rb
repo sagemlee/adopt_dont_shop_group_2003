875a6c09a215147431990ff984d7ac4096742f72
Sage Lee <sagemcklee@gmail.com>
2020-05-13 16:46:18 -0600

    Add flash message for update page, user story 6

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,19 +17,19 @@ class ReviewsController < ApplicationController
 
   def edit
     @review = Review.find(params[:review_id])
-    # if review.save
-    #   flash[:notice] = "Review saved"
-    #   redirect_to "/shelters/#{@current_shelter.id}"
-    # else
-    #   flash.now[:alert] = "Please fill out the title, rating and content to submit a review."
-    #   render :new
-    # end
+
   end
 
   def update
-    review = Review.find(params[:review_id])
-    review.update(review_params)
-    redirect_to "/shelters/#{review.shelter_id}"
+    @review = Review.find(params[:review_id])
+    shelter = Shelter.find(params[:shelter_id])
+    @review.update(review_params)
+    if @review.save
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash.now[:alert] = "Please fill out the title, rating and content to submit a review."
+      render :edit
+    end
   end
 
   private
--- spec/features/reviews/user_can_edit_shelter_reviews_spec.rb
+++ spec/features/reviews/user_can_edit_shelter_reviews_spec.rb
@@ -15,7 +15,7 @@ describe "user edits review" do
       end
 
       expect(current_path).to eq("/shelters/#{shelter.id}/reviews/#{review1.id}/edit")
-      # 
+      #
       # expect(page).to have_content(review1.title)
       # expect(page).to have_content(review1.rating)
       # expect(page).to have_content(review1.content)
@@ -31,6 +31,48 @@ describe "user edits review" do
 
       expect(page).to have_content("Great Shelter")
       expect(page).to have_content("Blah blah blah blah blah blah")
+    end
+
+    it "Shows flash message if fields are missing when submitting on edit page" do
+      shelter = create(:shelter)
+
+      review1 = create(:review, shelter_id: "#{shelter.id}")
+      review2 = create(:review, shelter_id: "#{shelter.id}")
+
+      visit "/shelters/#{shelter.id}"
+
+      within(".review-#{review1.id}")do
+        click_link "Edit this Review"
+      end
+
+      expect(current_path).to eq("/shelters/#{shelter.id}/reviews/#{review1.id}/edit")
+      #
+      # expect(page).to have_content(review1.title)
+      # expect(page).to have_content(review1.rating)
+      # expect(page).to have_content(review1.content)
+      # expect(page).to have_content(review1.img_url)
+
+      fill_in :title, with: "Great Shelter"
+      fill_in :rating, with: 4
+      fill_in :content, with: nil
+      fill_in :img_url, with: "www.img.com/1"
+
+      click_button "Submit Updates"
+
+      expect(page).to have_content("Please fill out the title, rating and content to submit a review")
+      expect(current_path).to eq("/shelters/#{shelter.id}/reviews/#{review1.id}")
+
+      fill_in :title, with: "Great Shelter"
+      fill_in :rating, with: 4
+      fill_in :content, with: "Something something something something"
+      fill_in :img_url, with: "www.img.com/1"
+
+      expect(current_path).to eq("/shelters/#{shelter.id}/reviews/#{review1.id}")
+
+      click_button "Submit Updates"
+
+      expect(page).to have_content("Great Shelter")
+      expect(page).to have_content("Something something something something")
     end
   end
 end
