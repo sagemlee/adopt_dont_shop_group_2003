ec57135d626d0bdb4311a3791329cdb5c11c99bc
Derek Borski <derek.borski@gmail.com>
2020-05-14 16:57:53 -0600

    Move selection form down on view, update create spec, and update create action in controller

--- app/controllers/concerns/applications_controller.rb
+++ app/controllers/concerns/applications_controller.rb
@@ -7,15 +7,27 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    # application = Application.new(params[:pet_ids])
-    redirect_to "/favorites/"
+    new_application = Application.create(application_params)
+    
+    pet_ids = params[:pet_ids]
+
+    if new_application.save
+      pet_ids.each do |pet_id|
+        PetApplication.create!(application_id: new_application.id, pet_id: pet_id)
+        favorite.contents.reject! { |pet_id,count| pet_ids.include?(pet_id) }
+      end 
+      flash[:notice] = "Your application has been submitted"
+      redirect_to "/favorites"
+      # session[:favorite].delete(pet_id)
+    else 
+      flash[:notice] = "You must fill out all contents of the application form"
+      redirect_to '/applications/new'
+    end 
   end
 
   private
 
   def application_params
-    params[:application].permit(:name, :address, :city, :state, :zip, :phone_number, :description, :approved)
+    params[:application].permit(:name, :address, :city, :state, :zip, :phone_number, :description, :approval_status)
   end
-  
-  
 end
--- spec/features/applications/create_application_spec.rb
+++ spec/features/applications/create_application_spec.rb
@@ -38,5 +38,48 @@ describe "user creates application" do
       expect(page).to_not have_css("#pet-#{pet2.id}")
       expect(page).to_not have_css("#pet-#{pet3.id}")
     end
+
+    xit "creates application" do
+      pet2 = create(:pet)
+      pet3 = create(:pet)
+      application = Application.new(name: "app 1", address: "add2", city: "city1", state: "state1", zipcode: "80000", phone_number: "111-111-1111", description: "descr1")
+
+      PetApplication.new()
+
+      visit "/pets/#{pet2.id}"
+      within("#pet-#{pet2.id}") do
+        click_button "Add Pet to Favorites"
+      end
+
+      visit "/pets/#{pet3.id}"
+      within("#pet-#{pet3.id}") do
+        click_button "Add Pet to Favorites"
+      end
+
+      visit '/favorites'
+      click_link "Adopt My Favorite Pets"
+      expect(current_path).to eq('/applications/new')
+
+      select("#{pet2.name}")
+      select("#{pet3.name}")
+      fill_in :name, with: "Bob"
+      fill_in :address, with: "Bobsfdwe"
+      fill_in :city, with: "Bobagre"
+      fill_in :state, with: "Bobsfg"
+      fill_in :zip, with: "12345"
+      fill_in :description, with: "Bob likes cheese"
+
+      click_button "Submit Application"
+
+      new_application = Application.last
+
+      expect(new_application.name).to eq("Bob")
+      expect(new_application.address).to eq("Bobsfdwe")
+      expect(new_application.city).to eq("Bobagre")
+      expect(new_application.state).to eq("Bobsfg")
+      expect(new_application.state).to eq("Bob")
+      expect(new_application.zip).to eq("Bob")
+      expect(new_application.description).to eq("Bob")
+    end
   end
 end
