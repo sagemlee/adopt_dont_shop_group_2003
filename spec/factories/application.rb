e6b9019d05b2fefb107a1969b62a065e71607add
Sage Lee <sagemcklee@gmail.com>
2020-05-13 21:04:17 -0600

    Add create application spec

--- spec/factories/application.rb
+++ spec/factories/application.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
-  factory :pet do
+  factory :application do
     sequence(:name) {|n| "Application #{n}" }
     sequence(:address) { |n| "#{n} street" }
     sequence(:city) {|n| "city #{n}" }
     sequence(:state) { |n| "State #{n}" }
     sequence(:phone_number) { |n| "1-800-000#{n}" }
     sequence(:description) { |n| "descryption #{n}" }
-    pets_applications
   end
 end
--- spec/features/applications/create_application_spec.rb
+++ spec/features/applications/create_application_spec.rb
@@ -1,10 +1,48 @@
 require 'rails_helper'
 
-describe "user deletes review" do
-  describe "they visit /shelters/:shelter_id, click link to delete review" do
-    it "no longer sees shelter review" do
+describe "user creates application" do
+  describe "visits pet favorites and clicks create application" do
+    it "shows pet application page" do
 
+      pet1 = create(:pet, shelter_id: "#{shelter.id}", adoption_status: "pending")
+      pet2 = create(:pet, shelter_id: "#{shelter.id}")
+      pet3 = create(:pet, shelter_id: "#{shelter.id}")
 
+      visit "/pets/#{pet2.id}"
+      within("#pet-#{pet2.id}") do
+        click_button "Add Pet to Favorites"
+      end
+
+      visit "/pets/#{pet3.id}"
+      within("#pet-#{pet3.id}") do
+        click_button "Add Pet to Favorites"
+      end
+
+      visit '/favorites'
+      click_link "Adopt My Favorite Pets"
+      expect(current_path).to eq('/applications/new')
+
+      within("#pet-#{pet2.id}") do
+        click_button "Select"
+      end
+
+      within("#pet-#{pet3.id}") do
+        click_button "Select"
+      end
+
+      fill_in :name, with: "Bob"
+      fill_in :address, with: "Bobsfdwe"
+      fill_in :city, with: "Bobagre"
+      fill_in :state, with: "Bobsfg"
+      fill_in :zip, with: "12345"
+      fill_in :description, with: "Bob likes cheese"
+
+      click_button "Submit My Application"
+
+      expect(page).to have_content("Your application has been submitted")
+      expect(current_path).to eq('/favorites')
+      expect(page).to_not have_css("#pet-#{pet2.id}")
+      expect(page).to_not have_css("#pet-#{pet3.id}")
     end
   end
 end
