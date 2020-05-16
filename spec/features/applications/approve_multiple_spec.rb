667861ee024f7a769082d92f89e5a4e5c906540f
Sage Lee <sagemcklee@gmail.com>
2020-05-16 17:26:30 -0600

    Add updates to PetApplicationsController

---
+++ spec/features/applications/approve_multiple_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "user creates application for more than one pet" do
+  describe "visits application show page" do
+    it "can approve any number of pets" do
+      pet1 = create(:pet)
+      pet2 = create(:pet)
+
+      application1 = create(:application)
+
+      PetApplication.create!(application: application1, pet: pet1)
+      PetApplication.create!(application: application1, pet: pet2)
+
+      visit "/applications/#{application1.id}"
+
+      within("#pet-#{pet1.id}") do
+        click_link "Approve Application for Pet"
+      end
+
+      visit "/applications/#{application1.id}"
+
+      within("#pet-#{pet2.id}") do
+        click_link "Approve Application for Pet"
+      end
+
+      expect(pet1.applications.approved_application).to eq(application1)
+      expect(pet2.applications.approved_application).to eq(application1)
+
+    end
+  end
+end
