965a8718965ae98234500d2f0b3188e37273882d
Derek Borski <derek.borski@gmail.com>
2020-05-15 23:24:35 -0600

    Add all applications spec

---
+++ spec/features/applications/all_applications_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "user visits pets/:pet_id" do
+  describe "clicks link to view applications for pet" do
+    it "displays a list of all applications for this pet" do
+
+      pet1 = create(:pet)
+        
+      application1 = create(:application)
+      application2 = create(:application)
+
+      PetApplication.create!(application: application1, pet: pet1) 
+      PetApplication.create!(application: application2, pet: pet1) 
+
+      visit "/pets/#{pet1.id}"
+
+      click_link "See applications"
+
+      expect(current_path).to eq('/pets/applications')
+
+      have_link("#{application1.name}", :href => "/applications/#{application1.id}")
+      have_link("#{application2.name}", :href => "/applications/#{application2.id}")
+    end 
+  end 
+end 
+
