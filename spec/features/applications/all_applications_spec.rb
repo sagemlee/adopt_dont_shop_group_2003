629c76b9ae97559c79965058ee154d2d3c9a7ecb
Derek Borski <derek.borski@gmail.com>
2020-05-16 00:10:36 -0600

    Add spec for pets with no aps and update index view

--- spec/features/applications/all_applications_spec.rb
+++ spec/features/applications/all_applications_spec.rb
@@ -22,7 +22,17 @@ describe "user visits pets/:pet_id" do
 
       have_link("#{application1.name}", :href => "/applications/#{application1.id}")
       have_link("#{application2.name}", :href => "/applications/#{application2.id}")
+    end
+    
+    it "displays that there are no applications for this pet when pet has no applications" do
+      pet1 = create(:pet)
+        
+      application1 = create(:application)
+
+      visit "/pets/#{pet1.id}/applications/"
+
+      expect(page).to have_content("This pet has no applications")
+      expect(page).to_not have_content(application1.name)
     end 
   end 
 end 
-
