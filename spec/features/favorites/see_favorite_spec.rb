153aa22ee9b2932b39bfad224139f37c69527c56
Sage Lee <sagemcklee@gmail.com>
2020-05-16 14:40:05 -0600

    Add final updates to see_favorite_pet_spec, tests passing

--- spec/features/favorites/see_favorite_spec.rb
+++ spec/features/favorites/see_favorite_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "user sees all favorites" do
   describe "they visit /favorites" do
-    xit "displays all favorite pets" do
+    it "displays all favorite pets" do
 
       pet1 = create(:pet)
       pet2 = create(:pet)
@@ -27,7 +27,7 @@ end
 
 describe "user visits favorites page" do
   describe "user has no favorite pets" do
-    xit "displays text saying user has no favorite pets" do
+    it "displays text saying user has no favorite pets" do
 
       visit "/favorites/"
 
@@ -39,9 +39,6 @@ end
 describe "user visits favorites index page" do
   describe "one or more applications have been created" do
     it "displays section of pets with applications" do
-      # @pet1 = create(:pet)
-      # @pet2 = create(:pet)
-      # @pet3 = create(:pet)
 
       pet1 = create(:pet)
       pet2 = create(:pet)
@@ -53,36 +50,6 @@ describe "user visits favorites index page" do
       PetApplication.create!(application: application1, pet: pet2)
       PetApplication.create!(application: application2, pet: pet3)
 
-      # visit "/pets/#{@pet1.id}"
-      # within("#pet-#{@pet1.id}") do
-      #   click_button "Add Pet to Favorites"
-      # end
-      #
-      # visit "/pets/#{@pet2.id}"
-      # within("#pet-#{@pet2.id}") do
-      #   click_button "Add Pet to Favorites"
-      # end
-      #
-      # visit "/pets/#{@pet3.id}"
-      # within("#pet-#{@pet3.id}") do
-      #   click_button "Add Pet to Favorites"
-      # end
-      #
-      # visit '/favorites'
-      # click_link "Adopt My Favorite Pets"
-      # expect(current_path).to eq('/applications/new')
-      #
-      # select("#{@pet2.name}")
-      # select("#{@pet3.name}")
-      # fill_in :name, with: "Bob"
-      # fill_in :address, with: "Bobsfdwe"
-      # fill_in :city, with: "Bobagre"
-      # fill_in :state, with: "Bobsfg"
-      # fill_in :zipcode, with: "12345"
-      # fill_in :phone_number, with: "111-111-1111"
-      # fill_in :description, with: "Bob likes cheese"
-      #
-      # click_button "Submit Application"
       visit '/favorites'
 
       within("#pets_with_applications-#{pet2.id}") do
@@ -90,18 +57,16 @@ describe "user visits favorites index page" do
         click_link("#{pet2.name}")
         expect(current_path).to eq("/pets/#{pet2.id}")
       end
-      
+
+      visit '/favorites'
+
       within("#pets_with_applications-#{pet3.id}") do
         expect(page).to have_link("#{pet3.name}")
         click_link("#{pet3.name}")
         expect(current_path).to eq("/pets/#{pet3.id}")
       end
 
-      # within("#pets_with_applications-#{pet3.id}") do
-      #   expect(page).to have_link("#{pet3.name}")
-      #   click_link("#{pet3.name}")
-      #   expect(current_path).to eq("/pets/#{pet3.id}")
-      # end
+      visit '/favorites'
 
       within("#pets_with_applications") do
         expect(page).to_not have_link("#{pet1.name}")
@@ -110,11 +75,3 @@ describe "user visits favorites index page" do
     end
   end
 end
-
-# User Story 18, List of Pets that have applications on them
-#
-# As a visitor
-# After one or more applications have been created
-# When I visit the favorites index page
-# I see a section on the page that has a list of all of the pets that have at least one application on them
-# Each pet's name is a link to their show page
