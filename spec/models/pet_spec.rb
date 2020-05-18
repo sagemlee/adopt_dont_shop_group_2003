1528fb2a03c8c62c81fd798dac32f806f47a26c0
Sage Lee <sagemcklee@gmail.com>
2020-05-18 16:14:50 -0600

    Update pet model to validate all fields

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe Pet, type: :model do
   describe "validations" do
-    it { should validate_presence_of :name, :img_url, :age, :sex, :description}
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :img_url}
+    it { should validate_presence_of :age}
+    it { should validate_presence_of :sex}
+    it { should validate_presence_of :description}
   end
   describe "relationships" do
     it { should belong_to :shelter }
