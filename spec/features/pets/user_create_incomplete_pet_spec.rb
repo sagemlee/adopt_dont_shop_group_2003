4cc44e3bc7f71687dc87b07c162f1cede8d27ba6
Sage Lee <sagemcklee@gmail.com>
2020-05-18 16:43:58 -0600

    Add updates to user_create_incomplete_pet_spec

--- spec/features/pets/user_create_incomplete_pet_spec.rb
+++ spec/features/pets/user_create_incomplete_pet_spec.rb
@@ -1,36 +1,42 @@
 require 'rails_helper'
 
-describe "user creates or updates pet with incomplete information" do
-    it "sees flash message with fields they've missed" do
+describe "user creates pet with incomplete information" do
+  it "sees flash message with fields they've missed" do
 
-      shelter = create(:shelter)
+    shelter = create(:shelter)
 
-      visit "/shelters/#{shelter.id}/pets"
-      click_link "Create Pet"
+    visit "/shelters/#{shelter.id}/pets"
+    click_link "Create Pet"
 
-      fill_in :name, with: "Rupert"
-      fill_in :img_url, with: "www.img.com/2"
-      fill_in :age, with: 10
-      fill_in :sex, with: "male"
-      # fill_in :description, with: "alright"
-      click_on "Create Pet"
+    fill_in :name, with: "Rupert"
+    fill_in :img_url, with: "www.img.com/2"
+    fill_in :age, with: 10
+    fill_in :sex, with: "male"
+    # fill_in :description, with: "alright"
+    click_on "Create Pet"
 
-      expect(page).to have_content("Description can't be blank")
+    expect(page).to have_content("Description can't be blank")
 
-      pet = create(:pet)
+  end
+end
 
-      visit "/pets/#{pet.id}"
-      click_link "Update Pet"
+describe "user updates pet with incomplete information" do
+  it "sees flash message with fields they've missed" do
 
-      expect(current_path).to eq("/pets/#{pet.id}/edit")
+    pet = create(:pet)
 
-      fill_in :name, with: "Update name"
-      fill_in :img_url, with: "www.img.com/2"
-      # fill_in :age, with: 10
-      fill_in :sex, with: "female"
-      fill_in :description, with: "really cute"
-      click_on "Update Pet"
-      expect(page).to have_content("Age can't be blank")
+    visit "/pets/#{pet.id}"
+    click_link "Update Pet"
 
-    end
+    expect(current_path).to eq("/pets/#{pet.id}/edit")
+
+    fill_in :name, with: "Update name"
+    fill_in :img_url, with: "www.img.com/2"
+    # fill_in :age, with: 10
+    fill_in :sex, with: "female"
+    fill_in :description, with: "really cute"
+    click_on "Update Pet"
+    expect(page).to have_content("Age can't be blank")
+
+  end
 end
