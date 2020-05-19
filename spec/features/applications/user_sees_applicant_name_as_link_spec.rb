cde3faa4ecdc4d473e2c4dee2e8c54f7c01b245a
Sage Lee <sagemcklee@gmail.com>
2020-05-18 19:17:49 -0600

    Add link from pet show page to application page

---
+++ spec/features/applications/user_sees_applicant_name_as_link_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user sees any applicant name" do
+  describe "user can click the applicant name" do
+    it "will be directed to application show page" do
+
+      shelter1 = create(:shelter)
+      pet1 = create(:pet, shelter_id:"#{shelter1.id}")
+      application = create(:application)
+      PetApplication.create!(application: application, pet: pet1, approved: "true")
+
+      visit "/pets/#{pet1.id}"
+      expect(page).to have_link("#{application.name}")
+
+    end
+  end
+end
