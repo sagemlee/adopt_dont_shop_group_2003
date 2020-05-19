ab843a26e519b5614e7f6aa34bec6119fc2ab5aa
Sage Lee <sagemcklee@gmail.com>
2020-05-19 11:57:06 -0600

    Add testing for shelter pets index page delete buttons

--- spec/features/pets/user_cannot_delete_pet_with_approved_app_spec.rb
+++ spec/features/pets/user_cannot_delete_pet_with_approved_app_spec.rb
@@ -1,29 +1,59 @@
 require 'rails_helper'
 
 describe "user tries to delete pet, that has an approved application" do
-    it "will not see delete button" do
+  it "on pets index page will not see delete button" do
 
-      pet1 = create(:pet)
-      pet2 = create(:pet)
+    pet1 = create(:pet)
+    pet2 = create(:pet)
 
-      application1 = create(:application)
+    application1 = create(:application)
 
-      PetApplication.create!(application: application1, pet: pet1)
-      PetApplication.create!(application: application1, pet: pet2)
+    PetApplication.create!(application: application1, pet: pet1)
+    PetApplication.create!(application: application1, pet: pet2)
 
-      visit "/applications/#{application1.id}"
+    visit "/applications/#{application1.id}"
 
-      within("#pet-#{pet2.id}") do
-        click_link "Approve Application for Pet"
-      end
+    within("#pet-#{pet2.id}") do
+      click_link "Approve Application for Pet"
+    end
 
-      visit '/pets'
+    visit '/pets'
 
-      within("#pet-#{pet2.id}") do
-        expect(page).to_not have_button("Delete Pet")
-      end
-
-      visit "/pets/#{pet2.id}"
+    within("#pet-#{pet2.id}") do
       expect(page).to_not have_button("Delete Pet")
     end
+
+    visit "/pets/#{pet2.id}"
+    expect(page).to_not have_button("Delete Pet")
+  end
+
+  it "on shelter pets index page will not see delete button" do
+
+    shelter1 = create(:shelter)
+
+    pet1 = create(:pet, shelter: shelter1)
+    pet2 = create(:pet, shelter: shelter1)
+
+    application1 = create(:application)
+
+    PetApplication.create!(application: application1, pet: pet1)
+    PetApplication.create!(application: application1, pet: pet2)
+
+    visit "/applications/#{application1.id}"
+
+    within("#pet-#{pet1.id}") do
+      click_link "Approve Application for Pet"
+    end
+
+    visit "/shelters/#{shelter1.id}/pets"
+
+    within("#pet-#{pet1.id}") do
+      expect(page).to_not have_button("Delete Pet")
+    end
+
+    within("#pet-#{pet2.id}") do
+      expect(page).to have_button("Delete Pet")
+    end
+
+  end
 end
