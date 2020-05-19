e4cb50a2bec86d7b25cb0113d7da8ce8bc8e26c1
Sage Lee <sagemcklee@gmail.com>
2020-05-19 16:01:43 -0600

    Add review limit of 10

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :review do
     sequence(:title) {|n| "Review #{n}" }
-    sequence(:rating) { |n| 1 + n }
+    sequence(:rating, (1..9).cycle) { |n| 1 + n }
     sequence(:content) {|n| "Content #{n}" }
     sequence(:img_url) { |n| "http://img.com/#{1 + n}" }
   end
