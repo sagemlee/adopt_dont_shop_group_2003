297b2ea53a820b6cefc9e1728dff2ed63bbb06cd
Derek Borski <derek.borski@gmail.com>
2020-05-19 08:22:52 -0600

    Add pets with approved applications to favorites index view

--- spec/features/favorites/see_favorite_spec.rb
+++ spec/features/favorites/see_favorite_spec.rb
@@ -71,7 +71,55 @@ describe "user visits favorites index page" do
       within("#pets_with_applications") do
         expect(page).to_not have_link("#{pet1.name}")
       end
+    end
+    it "displays section of pets with approved applications" do
 
+      pet1 = create(:pet)
+      pet2 = create(:pet)
+      pet3 = create(:pet)
+
+      application1 = create(:application)
+      application2 = create(:application)
+      application3 = create(:application)
+
+      PetApplication.create!(application: application1, pet: pet1)
+      PetApplication.create!(application: application2, pet: pet2)
+      PetApplication.create!(application: application3, pet: pet3, approved: "true")
+
+      visit '/favorites'
+
+      within("#pets_with_approved_apps-#{pet3.id}") do
+        expect(page).to have_link("#{pet3.name}")
+        click_link("#{pet3.name}")
+        expect(current_path).to eq("/pets/#{pet3.id}")
+      end
+
+      # visit '/favorites'
+
+      # within("#pets_with_applications-#{pet3.id}") do
+      #   expect(page).to have_link("#{pet3.name}")
+      #   click_link("#{pet3.name}")
+      #   expect(current_path).to eq("/pets/#{pet3.id}")
+      # end
+
+      visit '/favorites'
+
+      within("#pets_with_approved_apps") do
+        expect(page).to_not have_link("#{pet1.name}")
+      end
+
+      within("#pets_with_approved_apps") do
+        expect(page).to_not have_link("#{pet2.name}")
+      end
     end
   end
 end
+
+# User Story 37, List of Pets with Approved Applications
+
+# As a visitor
+# After an application has been approved for one or more pets
+# When I visit the favorites page
+# I see a section on the page that has a list of all of the pets 
+# that have an approved application on them
+# Each pet's name is a link to their show page
