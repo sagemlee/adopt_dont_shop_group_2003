e59a007c7e1b5b0f5a94c4807521c6cd6fcbab33
Derek Borski <derek.borski@gmail.com>
2020-05-18 22:39:15 -0600

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,8 +38,8 @@ review6 = shelter2.reviews.create(title: "Not good", rating: 4, content: "They n
 review7 = shelter3.reviews.create(title: "Best shelter around", rating: 10, content:"Fantastic service", img_url: "https://media.npr.org/assets/img/2013/02/06/dog_wide-e19af42dcba6ac82e35773015db5d04ef49c9beb-s1600-c85.jpg")
 review8 = shelter3.reviews.create(title: "Fantastic", rating: 9, content: "Telling all my friends", img_url: "https://media.npr.org/assets/img/2013/02/06/dog_wide-e19af42dcba6ac82e35773015db5d04ef49c9beb-s1600-c85.jpg")
 review9 = shelter3.reviews.create(title: "Well run", rating: 9, content: "Loved the staff here", img_url: "https://media.npr.org/assets/img/2013/02/06/dog_wide-e19af42dcba6ac82e35773015db5d04ef49c9beb-s1600-c85.jpg")
-review10 = shelter4.reviews.create(title: "Strange animals here", rating: 7, content: "Didn't know you could adopt possums!", img_url: "https://media.npr.org/assets/img/2013/02/06/dog_wide-e19af42dcba6ac82e35773015db5d04ef49c9beb-s1600-c85.jpg")
-review11 = shelter4.reviews.create(title: "Lizards?", rating: 8, content: "I take my lizard on a walk everyday!", img_url: "https://media.npr.org/assets/img/2013/02/06/dog_wide-e19af42dcba6ac82e35773015db5d04ef49c9beb-s1600-c85.jpg")
+review10 = shelter4.reviews.create(title: "Strange animals here", rating: 7, content: "Didn't know you could adopt possums!", img_url: "https://adoptdontshop.s3-us-west-1.amazonaws.com/possum.jpg")
+review11 = shelter4.reviews.create(title: "Lizards?", rating: 8, content: "I take my lizard on a walk everyday!", img_url: "https://adoptdontshop.s3-us-west-1.amazonaws.com/komodo.jpg")
 
 application1 = Application.create(name: "Bill", address: "321 Made Up Dr.", city: "Denver", state: "CO", zipcode: "80000", phone_number: "111-111-1111", description: "I want this pet!")
 application2 = Application.create(name: "Mary", address: "321 Made Up Dr.", city: "Denver", state: "CO", zipcode: "80000", phone_number: "111-111-1111", description: "I want this pet!")
