31a8cf889b4e44d0d53d9fb03d885d349b959265
Derek Borski <derek.borski@gmail.com>
2020-05-18 20:28:01 -0600

    Add number of applications on file for shelter in view

--- spec/features/shelters/shelter_statistics_spec.rb
+++ spec/features/shelters/shelter_statistics_spec.rb
@@ -9,6 +9,10 @@ describe "When user visits shelter show page" do
     pet2 = create(:pet, shelter_id: "#{shelter1.id}")
     pet3 = create(:pet, shelter_id: "#{shelter1.id}")
 
+    application1 = create(:application)
+
+    PetApplication.create!(pet: pet1, application: application1)
+
     visit "/shelters/#{shelter1.id}"
 
     within(".shelter-count") do
@@ -26,10 +30,28 @@ describe "When user visits shelter show page" do
     visit "/shelters/#{shelter1.id}"
 
      within(".shelter-average") do
-      expect(page).to have_content("Average Review Rating: #{shelter1.reviews.average_rating}")
+      expect(page).to have_content("Average Review Rating: #{shelter1.reviews.average_rating.round(2)}")
     end 
   end
   it "displays number of applications on file for that shelter" do
+
+    shelter1 = create(:shelter)
+
+    pet1 = create(:pet, shelter_id: "#{shelter1.id}")
+    pet2 = create(:pet, shelter_id: "#{shelter1.id}")
+    pet3 = create(:pet, shelter_id: "#{shelter1.id}")
+
+    application1 = create(:application)
+    application2 = create(:application)
+
+    PetApplication.create!(pet: pet1, application: application1)
+    PetApplication.create!(pet: pet1, application: application2)
+
+    visit "/shelters/#{shelter1.id}"
+
+     within(".shelter-apps") do
+      expect(page).to have_content("Number of Applications on File: #{shelter1.pets.number_of_apps}")
+    end 
   end
 end
 
