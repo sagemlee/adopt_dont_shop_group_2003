28a492b5b3ae9b1d32dee5460581c6b9fb272db0
Sage Lee <sagemcklee@gmail.com>
2020-05-18 18:41:04 -0600

    Add rspec for pet and shelter index page

--- spec/features/shelters/user_sees_link_for_every_shelter_name_spec.rb
+++ spec/features/shelters/user_sees_link_for_every_shelter_name_spec.rb
@@ -8,19 +8,34 @@ describe "user sees any shelter" do
       shelter2 = create(:shelter)
       shelter3 = create(:shelter)
 
+      pet1 = create(:pet, shelter_id:"#{shelter1.id}")
+      pet2 = create(:pet, shelter_id:"#{shelter2.id}")
+
+
       visit "/shelters"
 
       within("#shelter-#{shelter1.id}") do
-        expect(page)to.have_link("#{shelter1.name}")
+        expect(page).to have_link("#{shelter1.name}")
       end
 
       within("#shelter-#{shelter2.id}") do
-        expect(page)to.have_link("#{shelter2.name}")
+        expect(page).to have_link("#{shelter2.name}")
       end
 
       within("#shelter-#{shelter3.id}") do
-        expect(page)to.have_link("#{shelter3.name}")
+        expect(page).to have_link("#{shelter3.name}")
       end
+
+      visit "/pets"
+
+      within("#pet-#{pet1.id}") do
+        expect(page).to have_link("#{shelter1.name}")
+      end
+
+      within("#pet-#{pet2.id}") do
+        expect(page).to have_link("#{shelter2.name}")
+      end
+
     end
   end
 end
